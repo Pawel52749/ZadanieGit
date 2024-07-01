4cc653bfe6b1e70e042e722fac625f2fe03a1f82
Pawel52749 <1palakniz@gmail.com>
2024-07-01 14:14:12 +0200

    Dodanie flag z pojedynczym - i zmodyfikowanie help

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
     date
-elif [ "$1" == "--logs" ]; then
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
     liczba_logs=100
     if [ ! -z "$2" ]; then
         liczba_logs=$2
@@ -13,12 +13,12 @@ elif [ "$1" == "--logs" ]; then
         echo "Stworzony przez: $0" >> log_$i.txt
         date >> log_$i.txt
     done
-elif [ "$1" == "--help" ]; then
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo "Dostepne opcje:"
-    echo "  --date        : Wyswietla aktualna date"
-    echo "  --logs [nr]  :  Tworzy [nr] logow (domyslnie tworzy 100)"
-    echo "  --help        : Wyswietla help"
+    echo "  -d, --date        : Wyswietla aktualna date"
+    echo "  -l, --logs [nr]   : Tworzy [nr] logow (domyslnie tworzy 100)"
+    echo "  -h, --help        : Wyswietla help"
 else
-    echo "Niepoprawne dane uzyj --help"
+    echo "Niepoprawne dane uzyj --help lub -h"
 fi
 
