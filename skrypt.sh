e1bfa23cd0b74da2fbe8be5caea48bba0c6e2675
Pawel52749 <1palakniz@gmail.com>
2024-07-01 13:41:58 +0200

    Dodano flage --logs do skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    for i in {1..100}
+    do
+        echo "Nr log $i" > log_$i.txt
+        echo "Stworzone przez skrypt: $0" >> log_$i.txt
+        date >> log_$i.txt
+    done
 else
-    echo "Wpisz: $0 --date"
+    echo "Napisz: $0 --date"
+    echo "       $0 --logs"
 fi
+
 
